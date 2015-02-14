984ef85ddac1924e8bb2f4e51b9f573af95736e8
William McNichols <wmcnicho@usc.edu>
2015-02-14 16:32:34 +0000

    Wrote symbol classifer that creates a property matrix of the all the image properties

--- findSymbols.m
+++ findSymbols.m
@@ -1,8 +1,35 @@
-function [  ] = findSymbols(  )
-%findSymbols Summary of this function goes here
-%   Detailed explanation goes here
+function [  ] = findSymbols( regions, bin_image )
+%given the regions of the image, calculate the compactness of all of the
+%regions to determine which symbols are the card
 
 
+image_prop_matrix = zeros(length(regions), 3);
+%iterate over the bounding boxes of the regions
+
+for j = 1:length(regions)
+    
+    myVec = int32(regions(j).BoundingBox);% [x, y, w, h]
+    
+    xmin = myVec(1);
+    xmax = myVec(1) + myVec(3);
+    ymin = myVec(2);
+    ymax = myVec(2) + myVec(4);
+    
+    %pull out subimage from the boundingbox
+    %subimage = bin_image(133:138, 231:234)
+    subimage = bin_image(ymin:ymax, xmin:xmax);
+    figure(j);
+    imshow(subimage);
+    %get properties of the subimage
+    prop = getproperties(subimage);
+    %add these properties to a matrix
+    image_prop_matrix(j, :) = prop;
+    
+
+end
+
+%for now just print the matrix
+disp(image_prop_matrix);
 
 end
 
--- project1.m
+++ project1.m
@@ -1,14 +1,32 @@
 function [  ] = project1(  )
 
-for i = 1:32
-    image_name = strcat('train', num2str(i));
-    file_path = strcat('./data1/', image_name, '.jpg');
-    save_name = strcat('./Stage1_cardLoc/', image_name, '_out.jpg');
-    card = cardlocation(file_path, 0);
-    imwrite(card, save_name);
-    
-end
+% for i = 1:32
+%     image_name = strcat('train', num2str(i));
+%     file_path = strcat('./data1/', image_name, '.jpg');
+%     save_name = strcat('./Stage1_cardLoc/', image_name, '_out.jpg');
+%     card = cardlocation(file_path, 0);
+%     imwrite(card, save_name);
+%     
+% end
 
+% Load image
+I = imread('./data1/train1.jpg');
+i = myjpgload('./data1/train1.jpg',0);
+
+disp('Starting adapt. Please wait.');
+ad = adapt('./data1/train1.jpg');
+disp('Adapt finished.');
+
+morph = bwmorph(ad,'open',16);
+%imshow(morph);
+
+%imshow(ad);
+
+% Finds regions
+label = bwlabel(morph,4);
+regions = regionprops(label,['basic']);
+
+findSymbols(regions, morph);
 
 end
 
