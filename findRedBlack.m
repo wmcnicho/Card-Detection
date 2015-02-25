4a11788e3a44d66c9e2544e67e961c9c74d0ed7c
William McNichols <wmcnicho@usc.edu>
2015-02-25 17:52:26 +0000

    Added a ton of comments and moved old files to Deprecated

--- findRedBlack.m
+++ findRedBlack.m
@@ -1,38 +1,46 @@
 function [isRed] = findRedBlack(image,regions, props)
+%FINDREDBLACK Given an image, its associated regions struct from
+%regionprops, and its property matrix, this function will return 1 if the
+%image is a red card and 0 if it is black card.
+%   This funciton converts the image into a normalized RGB image and then
+%   sums the total number of red pixels in a symbol subregion of the image.
+%   If the ratio of red pixels to other pixels in the subimage is greater
+%   than a threshold the image is assumed to be red. (default threshold is
+%   .45 and was determined experimentally)
     
-    image = double(image);
+threshold = 0.45;
+image = double(image);
 
-    R = image(:,:,1)+1;
-    G = image(:,:,2)+1;
-    B = image(:,:,3)+1;
+%Get the total number of red, green, and blue pixel values
+R = image(:,:,1)+1;
+G = image(:,:,2)+1;
+B = image(:,:,3)+1;
+%Normalize the red, blue, and green value
+NormalizedRed = R(:,:)./sqrt(R(:,:).^2+G(:,:).^2+B(:,:).^2);
+NormalizedGreen = G(:,:)./sqrt(R(:,:).^2+G(:,:).^2+B(:,:).^2);
+NormalizedBlue = B(:,:)./sqrt(R(:,:).^2+G(:,:).^2+B(:,:).^2);
+%put the normalized rgb values into norm
+norm(:,:,1) = NormalizedRed(:,:);
+norm(:,:,2) = NormalizedGreen(:,:);
+norm(:,:,3) = NormalizedBlue(:,:);
 
-    NormalizedRed = R(:,:)./sqrt(R(:,:).^2+G(:,:).^2+B(:,:).^2);
-    NormalizedGreen = G(:,:)./sqrt(R(:,:).^2+G(:,:).^2+B(:,:).^2);
-    NormalizedBlue = B(:,:)./sqrt(R(:,:).^2+G(:,:).^2+B(:,:).^2);
+%extract a subimage corresponding to the smallest region (which should
+%always be a symbol in our implementation)
+[~,index] = min(props(:,2));
+region_index = props(index,1);
+myVec = int32(regions(region_index).BoundingBox);% [x, y, w, h]
+xmin = myVec(1);
+xmax = myVec(1) + myVec(3);
+ymin = myVec(2);
+ymax = myVec(2) + myVec(4);
+subimage = norm(ymin:ymax, xmin:xmax,:);
 
-    norm(:,:,1) = NormalizedRed(:,:);
-    norm(:,:,2) = NormalizedGreen(:,:);
-    norm(:,:,3) = NormalizedBlue(:,:);
-    
-    %imshow(norm);
-    
-    [value,index] = min(props(:,2));
-    
-    region_index = props(index,1);
-    
-    myVec = int32(regions(region_index).BoundingBox);% [x, y, w, h]
-    
-    xmin = myVec(1);
-    xmax = myVec(1) + myVec(3);
-    ymin = myVec(2);
-    ymax = myVec(2) + myVec(4);
-    
-    subimage = norm(ymin:ymax, xmin:xmax,:);
-    
-    R = sum(sum(subimage(:,:,1)));
-    G = sum(sum(subimage(:,:,2)));
-    B = sum(sum(subimage(:,:,3)));
-    
-    isRed = ((R/ (R+G+B)) > .45);
-    
+%sum the total pixel values of the normalised sub_image
+sub_R = sum(sum(subimage(:,:,1)));
+sub_G = sum(sum(subimage(:,:,2)));
+sub_B = sum(sum(subimage(:,:,3)));
+
+%see if the ratio of red to total colors is above a threshold
+isRed = ((sub_R/ (sub_R+sub_G+sub_B)) > threshold);
+
 end
--- trainred.m
+++ trainred.m
@@ -1,16 +1,21 @@
 function [] = trainred()
+%TRAINRED Used to train the red bayesian classifer model. Assumes that
+%there is data prestored in properties/heart and properties/diamond
+%according to the convention specified in readSymProps
+%   The file reading was used to speed up the training since the symbol
+%   dectection process can be time consuming
 
-    symbolVecs = readSymProps('heart');
-    symbolVecs = [symbolVecs;readSymProps('diamond')];
-    
-    redDim = 3;
-    redNumClass = 2;
-    symbolClasses = [ones(60,1);2 * ones(60,1)];
-    
-    symbol_n = size(symbolClasses,1);
-    
-    [redMeans,redInvcors,redAprioris] = buildmodel(redDim,symbolVecs,symbol_n,redNumClass,symbolClasses);
-    
-    eval(['save  trained_models/symbols_red.mat redNumClass redMeans redInvcors redAprioris']);
+symbolVecs = readSymProps('heart');
+symbolVecs = [symbolVecs;readSymProps('diamond')];
+
+redDim = 3;
+redNumClass = 2;
+symbolClasses = [ones(60,1);2 * ones(60,1)];
+
+symbol_n = size(symbolClasses,1);
+
+[redMeans,redInvcors,redAprioris] = buildmodel(redDim,symbolVecs,symbol_n,redNumClass,symbolClasses);
+
+eval(['save  trained_models/symbols_red.mat redNumClass redMeans redInvcors redAprioris']);
     
 end
